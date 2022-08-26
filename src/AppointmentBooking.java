f885a2740a3ec0a77fc9c005e5ed5183efcecae5
Ablaze99 <femiemma99@gmail.com>
2022-08-26 22:32:07 +0100

    Hospital

--- src/AppointmentBooking.java
+++ src/AppointmentBooking.java
@@ -1,5 +1,24 @@
 import java.util.ArrayList;
 
 public class AppointmentBooking {
-    ArrayList<String> doctorName=new ArrayList<String>(git);
+    ArrayList<String> doctorName=new ArrayList<String>();
+    ArrayList<String> doctorQualification=new ArrayList<String>();
+    ArrayList<String> doctorGender=new ArrayList<String>();
+    ArrayList<String> doctorPassword=new ArrayList<String>();
+    ArrayList<Integer> doctorAge=new ArrayList<Integer>();
+    ArrayList<Long> doctorMobileNumber=new ArrayList<Long>();
+    ArrayList<String> doctorCity=new ArrayList<String>();
+
+    ArrayList<Integer> patientAge=new ArrayList<Integer>();
+    ArrayList<String> patientName=new ArrayList<String>();
+    ArrayList<String> patientGender=new ArrayList<String>();
+    ArrayList<String> patientPassword=new ArrayList<String>();
+    ArrayList<String> patientConfirmPassword=new ArrayList<String>();
+    ArrayList<Long> patientMobileNumber=new ArrayList<Long>();
+
+    ArrayList<Integer> patientAgeBooking=new ArrayList<Integer>();
+    ArrayList<String> patientNameBooking=new ArrayList<String>();
+    ArrayList<String> patientGenderBooking=new ArrayList<String>();
+    ArrayList<Byte> doctorID=new ArrayList<Byte>();
+
 }
