c2078edd7a35fd058076f95f387f47c30e026ab4
Ablaze99 <femiemma99@gmail.com>
2022-08-27 11:49:11 +0100

    Hospital

--- src/AppointmentBooking.java
+++ src/AppointmentBooking.java
@@ -1,25 +1,27 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AppointmentBooking {
-    ArrayList<String> doctorName=new ArrayList<String>();
-    ArrayList<String> doctorQualification=new ArrayList<String>();
-    ArrayList<String> doctorGender=new ArrayList<String>();
-    ArrayList<String> doctorPassword=new ArrayList<String>();
-    ArrayList<Integer> doctorAge=new ArrayList<Integer>();
-    ArrayList<Long> doctorMobileNumber=new ArrayList<Long>();
-    ArrayList<String> doctorCity=new ArrayList<String>();
+    ArrayList<String> doctorName = new ArrayList<String>();
+    ArrayList<String> doctorQualification = new ArrayList<String>();
+    ArrayList<String> doctorGender = new ArrayList<String>();
+    ArrayList<String> doctorPassword = new ArrayList<String>();
+    ArrayList<Integer> doctorAge = new ArrayList<Integer>();
+    ArrayList<Long> doctorMobileNumber = new ArrayList<Long>();
+    ArrayList<String> doctorCity = new ArrayList<String>();
 
-    ArrayList<Integer> patientAge=new ArrayList<Integer>();
-    ArrayList<String> patientName=new ArrayList<String>();
-    ArrayList<String> patientGender=new ArrayList<String>();
-    ArrayList<String> patientPassword=new ArrayList<String>();
-    ArrayList<String> patientConfirmPassword=new ArrayList<String>();
-    ArrayList<Long> patientMobileNumber=new ArrayList<Long>();
+    ArrayList<Integer> patientAge = new ArrayList<Integer>();
+    ArrayList<String> patientName = new ArrayList<String>();
+    ArrayList<String> patientGender = new ArrayList<String>();
+    ArrayList<String> patientPassword = new ArrayList<String>();
+    ArrayList<String> patientConfirmPassword = new ArrayList<String>();
+    ArrayList<Long> patientMobileNumber = new ArrayList<Long>();
 
-    ArrayList<Integer> patientAgeBooking=new ArrayList<Integer>();
-    ArrayList<String> patientNameBooking=new ArrayList<String>();
-    ArrayList<String> patientGenderBooking=new ArrayList<String>();
-    ArrayList<Byte> doctorID=new ArrayList<Byte>();
+    ArrayList<Integer> patientAgeBooking = new ArrayList<Integer>();
+    ArrayList<String> patientNameBooking = new ArrayList<String>();
+    ArrayList<String> patientGenderBooking = new ArrayList<String>();
+    ArrayList<Byte> doctorID = new ArrayList<Byte>();
+    private int i;
 
     void dataAdd() { //methods for creating default doctor list present in database.
 
@@ -28,7 +30,7 @@ public class AppointmentBooking {
         doctorName.add("Jude Osas");
         doctorName.add("Paul Walker");
         doctorName.add("Femi ojo");
-        doctorName.add("Mike Dean");
+        doctorName.add("Mike Todd");
         doctorName.add("John Micheal");
 
         doctorQualification.add("MBBS");
@@ -81,4 +83,44 @@ public class AppointmentBooking {
 
     }
 
-}
+    void admin() throws Exception {
+        Scanner sc = new Scanner(System.in);
+        byte choice;
+        boolean flag = true;
+
+        while (flag) {
+            new ProcessBuilder("cmd", "/c").inheritIO().start().waitFor();
+            System.out.println("\t\t________________________");
+            System.out.println("\t\t|                        |");
+            System.out.println("\t\t|     Welcome Admin      |");
+            System.out.println("\t\t________________________");
+            System.out.print("\n\n1.Doctor List \t2. Registered Patients \t3. Patients with Appointment \n\nEnter Your Choice");
+            choice = sc.nextByte();
+            switch (choice) {
+                case 1:
+                    for (int i = 0; i <= doctorName.size() - 1; i++) ;
+                {
+                    System.out.println("Name: \t" + doctorName.get(i));
+                }
+                byte menuChoice;
+                System.out.println("\n\n1. Main menu\t 2. Previous menu");
+                menuChoice = sc.nextByte();
+                if (menuChoice == 1) {
+                    flag = false;
+                } else {
+                    break;
+                }
+                break;
+                case 2:
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+
+
+
+
+
